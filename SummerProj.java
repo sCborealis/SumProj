12b36ea7d87585d86399f647002169f8edb96798
sCborealis <stellacoronaborealis@gmail.com>
2026-05-08 04:49:15 +0100

    Restarting

--- SummerProj.java
+++ SummerProj.java
@@ -2,19 +2,33 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;   
           
-public class SummerProj {
-  public static void main(String[] args) {
-    File myObj = new File("Metrolink_times_linecolour(in).csv");
 
-    try (Scanner myReader = new Scanner(myObj)) {
-      while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        System.out.println(data);
+
+public class SummerProj {
+  public static void main(String[] args) throws java.io.FileNotFoundException {
+    
+  
+    Scanner firstStation = new Scanner(System.in);
+
+    System.out.println("Enter starting train station: ");
+    String startStation = firstStation.nextLine();
+
+    
+    File timeFile = new File("Metrolink_times_linecolour(in).csv");
+    try (Scanner myReader = new Scanner(timeFile)) {
+      int count=0;
+      while (count == 0){
+        while (myReader.hasNextLine()) {
+          String data = myReader.nextLine();
+          if (data.contains(startStation) == true) {
+            count++;
+            break;
+          }
+        }
+        System.out.println("Enter a valid station:");
+        startStation = firstStation.nextLine();
+
       }
-    } catch (FileNotFoundException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
     }
   }
-
 }
---
+++ metro.java
@@ -0,0 +1,7 @@
+//use a map
+import java.util;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class metroMap
