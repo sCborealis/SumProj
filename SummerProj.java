d7753f67c9defe918acff27ce2c91a34734f0c34
sCborealis <stellacoronaborealis@gmail.com>
2026-05-08 16:11:01 +0100

    User input of stations

--- SummerProj.java
+++ SummerProj.java
@@ -1,34 +1,43 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;   
-          
 
 
 public class SummerProj {
+
   public static void main(String[] args) throws java.io.FileNotFoundException {
-    
   
     Scanner firstStation = new Scanner(System.in);
-
-    System.out.println("Enter starting train station: ");
-    String startStation = firstStation.nextLine();
-
-    
+    metro metromap = new metro();
     File timeFile = new File("Metrolink_times_linecolour(in).csv");
-    try (Scanner myReader = new Scanner(timeFile)) {
-      int count=0;
-      while (count == 0){
-        while (myReader.hasNextLine()) {
-          String data = myReader.nextLine();
-          if (data.contains(startStation) == true) {
-            count++;
-            break;
-          }
-        }
-        System.out.println("Enter a valid station:");
-        startStation = firstStation.nextLine();
 
+    Scanner inputStation = new Scanner(System.in);
+    boolean validStation = false;
+    
+    while (!validStation){ 
+      System.out.println("Enter starting train station: ");
+      String startStation = inputStation.nextLine();
+      if (validStation(metromap, startStation)){
+        validStation = true;
+      }else {
+        System.out.println(startStation + "is not a station");
       }
+    System.out.println("Valid station");
     }
+
+    boolean validStation = false;
+    
+    while (!validStation){ 
+      System.out.println("Enter ending train station: ");
+      String finalStation = inputStation.nextLine();
+      if (validStation(metromap, finalStation)){
+        validStation = true;
+      }else {
+        System.out.println(finalStation + "is not a station");
+      }
+    System.out.println("Valid end station");
+    }
+
+
   }
 }
